4d2b8c8878322efc5c10ac8c71d4b497a2178185
Natypou <naty_pou@hotmail.com>
2022-10-27 18:37:33 -0300

    frontend

---
+++ Backend/src/main/java/com/portfolio/Naty/NatyApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.Naty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatyApplication.class, args);
+	}
+
+}
